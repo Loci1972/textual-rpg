31fcf077efb744302f85fa6d7c81197ee8b54669
Loci1972 <toguyeninidja@gmail.com>
2026-04-24 19:36:23 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-clang++ -Wall -Wextra -std=c++11 -I./headers main.cpp src/player.cpp src/enemy.cpp src/tools.cpp -o rpg_game
+#!/bin/sh
+clang++ -Wall -Wextra -std=c++11 -I./headers main.cpp src/player.cpp src/enemy.cpp src/tools.cpp src/item.cpp src/game.cpp -o rpg_game
 ./rpg_game
