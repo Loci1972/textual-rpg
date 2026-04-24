624db28a1848d146955948d39f1ff81b2875af72
Loci1972 <toguyeninidja@gmail.com>
2026-04-24 20:43:57 +0000

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -31,6 +31,12 @@ std::string getString(){
     return text;
 }
 
+int realDamage(const Enemy& enemy, const Player& player, const std::string& entity){
+    if (entity == "player") return std::max(0, player.getAttack() - enemy.getDefense());
+    else if (entity == "enemy") return std::max(0, enemy.getAttack() - player.getDefense());
+    return 0;
+}
+
 bool randomState () {
     std::random_device rd; 
     std::mt19937 gen(rd()); 
@@ -38,4 +44,11 @@ bool randomState () {
     int random_num = distrib(gen);
     if (random_num == 1) return true;
     else return false;
+}
+
+int randomInt(int min, int max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(min, max);
+    return distrib(gen);
 }
