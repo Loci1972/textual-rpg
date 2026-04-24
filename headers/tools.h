ae0d9497c18faab3033964fadb423e74ee0f423d
Loci1972 <toguyeninidja@gmail.com>
2026-04-24 19:37:23 +0000

    Update tools.h

--- headers/tools.h
+++ headers/tools.h
@@ -3,16 +3,12 @@
 
 #include <random>
 #include <ctime>
-#include "player.h"
-#include "enemy.h"
 #include <iostream>
 #include <numeric>
 #include <string>
 
 int getNumber();
 std::string getString();
-int realDamage(const Enemy& enemy, const Player& player, const std::string& entity);
 bool randomState ();
-bool options(Player& player, Enemy& enemy, int& choice,bool& playerTurn,int& menueType);
 
 #endif
